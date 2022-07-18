3f5ddd7055fa752c9334b0adb7e2e4c0551444d0
Michael Bisgaard Olesen <michaelbisgaardo@gmail.com>
2022-07-18 21:51:13 +0200

    Parameterize using env vars

--- main.go
+++ main.go
@@ -6,38 +6,43 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
+	"strconv"
 )
 
 func main() {
-	listenPort := 8028
-	proxyUrl := "http://localhost:8080"
+	listenPort, err := strconv.Atoi(os.Getenv("LISTEN_PORT"))
+	if err != nil {
+		log.Fatalln(err)
+	}
+	proxyUrl := os.Getenv("PROXY_URL")
 
 	http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
 		client := &http.Client{}
 
 		reqBody, err := ioutil.ReadAll(req.Body)
 		if err != nil {
-			log.Fatalln(err)
+			log.Println(err)
 		}
 
 		proxyReq, err := http.NewRequest(req.Method, proxyUrl+req.URL.Path, bytes.NewReader(reqBody))
 		if err != nil {
-			log.Fatalln(err)
+			log.Println(err)
 		}
 		proxyReq.Header = req.Header
 
 		proxyRes, err := client.Do(proxyReq)
 		if err != nil {
-			log.Fatalln(err)
+			log.Println(err)
 		}
 		defer proxyRes.Body.Close()
 
 		resBody, err := ioutil.ReadAll(proxyRes.Body)
 		if err != nil {
-			log.Fatalln(err)
+			log.Println(err)
 		}
 		if _, err := w.Write(resBody); err != nil {
-			log.Fatalln(err)
+			log.Println(err)
 		}
 
 		fmt.Printf("%s: %s\t=>\t%s\n", req.URL.Path, reqBody, resBody)
